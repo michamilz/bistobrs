05cbc4202cec146e0858a5ac43172328d899c242
Michael Milz <hallo@michamilz.de>
2017-11-12 21:15:33 +0100

    colored icons

---
+++ icons/svg2icons.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+inkscape -z -e 512x512.png -w 512 -h 512 icon.svg
+inkscape -z -e 128x128.png -w 128 -h 128 icon.svg
+inkscape -z -e 48x48.png -w 48 -h 48 icon.svg
+inkscape -z -e 38x38.png -w 38 -h 38 icon.svg
+inkscape -z -e 19x19.png -w 19 -h 19 icon.svg
+inkscape -z -e 16x16.png -w 16 -h 16 icon.svg
